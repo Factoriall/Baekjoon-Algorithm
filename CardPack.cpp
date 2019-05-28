69eaa1c4211e5b26b1560188b5091980b9b38cc9
Factoriall <47839550+Factoriall@users.noreply.github.com>
2019-05-28 16:45:07 +0900

    Update CardPack.cpp

--- CardPack.cpp
+++ CardPack.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 using namespace std;
 
-int card[100001];
-int usedPnt[500001];
+int card[100001];//카드팩 정렬 
+int usedPnt[500001];//중복 확인 및 위치 확인
 
-void reset() {
+void reset() {//usedPnt 리셋 함수
 	int j;
 	for (j = 1; j <= 500000; j++) {
 		usedPnt[j] = 0;
@@ -24,6 +23,7 @@ int main() {
 		cin >> card[i];
 	}
 
+	//이진탐색 사용
 	int left = 1;
 	int right = N / M;
 	
@@ -35,25 +35,25 @@ int main() {
 		reset();
 
 		for (i = 1; i <= N; i++) {
-			if (usedPnt[card[i]] != 0 && usedPnt[card[i]] >= start) {
-				start = usedPnt[card[i]] + 1;
+			if (usedPnt[card[i]] != 0 && usedPnt[card[i]] >= start) {//만약 이미 사용한 적이 있고 && start지점보다 뒤에 있다. -> 중복된다.
+				start = usedPnt[card[i]] + 1;//스타트 지점 변경
 			}
 			
 			usedPnt[card[i]] = i;
 			
-			if (i + 1 - start == mid) {
+			if (i + 1 - start == mid) {//만약 실제 수랑 일치한다면 카운트 업
 				cnt++;
 				start = i + 1;
 			}
-			if (cnt == M)
+			if (cnt == M)//만약 수 일치하면 빠져나감
 				break;
 		}
 
-		if (cnt == M) {
+		if (cnt == M) {//만약 조건 만족하면 ans 값 대조, left 값 증가
 			ans = max(ans, mid);
 			left = mid + 1;
 		}
-		else {
+		else {//만족 안하면 right 값 줄이기
 			right = mid - 1;
 		}
 	}
