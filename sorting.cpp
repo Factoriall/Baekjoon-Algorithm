4dbb86ea85f7cfcc4e000977336a3b20f5f4dae0
Factoriall <47839550+Factoriall@users.noreply.github.com>
2019-05-29 11:57:15 +0900

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int arr[100000];
+int n;
+
+int find() {
+	int i;
+	int cnt = 0;
+	int down = 0;
+
+
+	for (i = 0; i < n-1; i++) {
+		if (arr[i] > arr[i + 1]) {//뒷 수가 더 작은 경우
+			down++;
+			if (down > 1) {
+				return 0;
+			}
+
+			if (i < n - 2 && arr[i] <= arr[i + 2])
+				cnt++;
+			if (i > 0 && arr[i - 1] <= arr[i + 1])
+				cnt++;
+			if (i == n - 2)
+				cnt++;
+			if (i == 0)
+				cnt++;
+		}
+	}
+
+	if (down == 0)
+		cnt = n;
+
+	return cnt;
+}
+
+int main() {
+	int i;
+	int result;
+
+	cin >> n;
+
+	for (i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	result = find();
+
+	cout << result << endl;
+
+	return 0;
+}
+
+
