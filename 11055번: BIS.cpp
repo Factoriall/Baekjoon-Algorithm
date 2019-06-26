7896c7f389163589bbb6840c6ef88683a7a53614
Factoriall <47839550+Factoriall@users.noreply.github.com>
2019-06-26 16:16:13 +0900

    Update 11055번: BIS.cpp

--- 11055번: BIS.cpp
+++ 11055번: BIS.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 	int n;
-	int arr[1001];//100
-	int dp[1001];
+	int arr[1001];//저장용 배열
+	int dp[1001];//메모이제이션 용 배열
 	int i, j;
 	int ans = 0;
 
@@ -14,6 +14,8 @@ int main() {
 		cin >> arr[i];
 	}
 
+	//위치 i 에 대해서 그 전 위치 j랑 arr 비교, arr[i]>arr[j]면 증가함수 성립
+	//dp[i]엔 그 증가함수가 성립되는 것 중 가장 큰 것을 저장한다.
 	for (i = 0; i < n; i++) {
 		int maxNum = 0;
 		for (j = 0; j < i; j++) {
@@ -22,7 +24,7 @@ int main() {
 			}
 		}
 		dp[i] = maxNum + arr[i];
-		ans = max(ans, dp[i]);
+		ans = max(ans, dp[i]);//항상 끝의 것이 맞는 것이 아니므로 할 때마다 체크!
 	}
 
 	/*for (i = 0; i < n; i++) {
