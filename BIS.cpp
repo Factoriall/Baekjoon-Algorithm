f64167f25573ef157510c5e2971dc07430618623
Factoriall <47839550+Factoriall@users.noreply.github.com>
2019-06-26 15:58:53 +0900

    Add files via upload

---
+++ BIS.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	int arr[1001];
+	int dp[1001];
+	int i, j;
+	int ans = 0;
+
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (i = 0; i < n; i++) {
+		int maxNum = 0;
+		for (j = 0; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				maxNum = max(maxNum, dp[j]);
+			}
+		}
+		dp[i] = maxNum + arr[i];
+		ans = max(ans, dp[i]);
+	}
+
+	/*for (i = 0; i < n; i++) {
+		cout << dp[i] << '\t';
+	}
+	cout << '\n';*/
+	cout << ans << '\n';
+
+	return 0;
+}
