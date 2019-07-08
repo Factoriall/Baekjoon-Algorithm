7645065b22c69dffc4e84a39b39306a1dbf4fd1c
Factoriall <47839550+Factoriall@users.noreply.github.com>
2019-07-09 01:06:50 +0900

    Update and rename Assignment.cpp to 13904번: Assignment.cpp

--- Greedy Algorithm/13904번: Assignment.cpp
+++ Greedy Algorithm/13904번: Assignment.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 pair<int, int> assign[1001];
 
-bool schedule[1001];
+bool schedule[1001];//방문 확인
 
-bool Pred(pair<int, int> a, pair<int, int> b) {
+bool Pred(pair<int, int> a, pair<int, int> b) {//정렬 방법 - 일단 점수 높은거 부터, 같으면 데드라인이 짧은 순 
 	if (a.second != b.second)
 		return a.second > b.second;
 	else
@@ -25,13 +25,13 @@ int main() {
 
 	int full = d;
 
-	sort(assign, assign + d, Pred);
+	sort(assign, assign + d, Pred);//정렬
 
 	for (i = 0; i < d; i++)
 		cout << assign[i].first << ' ' << assign[i].second << endl;
 	cout << endl;
 
-	for (i = 0; i < d; i++) {
+	for (i = 0; i < d; i++) {//이후 순서대로 과제를 제출할 수 있는 가장 늦은 날짜에 제출한다.
 		int latest = assign[i].first;
 
 		while (schedule[latest]) {
